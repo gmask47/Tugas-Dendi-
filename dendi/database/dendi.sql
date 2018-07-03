ed76442471112848c9b20c912c2f58597357bebf
gmask47 <39257257+gmask47@users.noreply.github.com>
2018-07-03 10:33:02 +0700

    Add files via upload

---
+++ dendi/database/dendi.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 03, 2018 at 05:01 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dendi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(10) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'dendi', 'guswandi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+CREATE TABLE `data` (
+  `nim` int(10) NOT NULL,
+  `n_mahasiswa` varchar(20) NOT NULL,
+  `k_mahasiswa` varchar(10) NOT NULL,
+  `grade` varchar(5) NOT NULL,
+  `tugas` int(5) NOT NULL,
+  `absen` int(3) NOT NULL,
+  `uts` int(3) NOT NULL,
+  `uas` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `data`
+--
+
+INSERT INTO `data` (`nim`, `n_mahasiswa`, `k_mahasiswa`, `grade`, `tugas`, `absen`, `uts`, `uas`) VALUES
+(17140018, 'Dendi Guswandi', '17.8A.33', 'A', 99, 99, 99, 99),
+(17148977, 'Hilda', '17.8A.33', 'B', 68, 60, 80, 70),
+(171408927, 'Kent', '17.8A.33', 'B', 70, 79, 60, 60),
+(171408978, 'Bram', '17.8A.33', 'B', 70, 89, 70, 60),
+(171409892, 'Brandont', '17.8A.33', 'B', 60, 80, 70, 60),
+(171498962, 'Sandi', '17.8A.33', 'C', 50, 50, 50, 50);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
